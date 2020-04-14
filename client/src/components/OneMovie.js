a349ef313d85367af6b52e0bcc40b57d13754d7e
konstantin0s <constantintofan85@gmail.com>
2020-04-14 21:26:18 +0200

    Style movie list

--- client/src/components/OneMovie.js
+++ client/src/components/OneMovie.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import './css/onemovie.css';
+
 
  class OneMovie extends Component {
 constructor(props) {
@@ -40,25 +49,75 @@ constructor(props) {
 console.log(this.state.movie);
         const { movie} = this.state;
 console.log(movie);
-        return (
 
-            <div>
-             <div className="card text-center" key={movie._id}>
-  <div className="card-body">
-    <img alt="Movie" className="movie-img" src={movie.image} />
-    <h2 className="card-title">{movie.title}</h2>
-       <p className="description">
-         {movie.description}
-    </p> 
-  </div>
+        return (
+            
+            <Card className="cardex">
+             <CardActionArea>
+            <CardContent className="image-card">
+              <CardMedia
+                component="img"
+                alt="Movie"
+                height="400"
+                image={movie.image}
+                title={movie.title}
+              />
+            </CardContent>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                Description
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+               {movie.description}
+                </Typography>
+              </CardContent>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                Director
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+               {movie.director}
+                </Typography>
+              </CardContent>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                Stars
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+               {movie.stars}
+                </Typography>
+              </CardContent>
+
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Showtimes:
+              </Button>
+              <Button size="small" color="primary">
+               {movie.showtimes}
+              </Button>
+            </CardActions>
+          </Card>
+
+
+
+//             <div>
+//              <div className="card text-center" key={movie._id}>
+//   <div className="card-body">
+//     <img alt="Movie" className="movie-img" src={movie.image} />
+//     <h2 className="card-title">{movie.title}</h2>
+//        <p className="description">
+//          {movie.description}
+//     </p> 
+//   </div>
 
   
-  {/* <div className="card-footer text-muted">
-     <span> Posted: {Moment(article.date.dateFrom).format('YYYY-MM-DD')}</span> 
-  </div> */}
+//   {/* <div className="card-footer text-muted">
+//      <span> Posted: {Moment(article.date.dateFrom).format('YYYY-MM-DD')}</span> 
+//   </div> */}
 
-</div>
-            </div>
+// </div>
+//             </div>
         )
     }
 }
