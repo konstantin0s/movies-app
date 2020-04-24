5191f293d39db8684570fe8fec72d7e42e327bf6
konstantin0s <constantintofan85@gmail.com>
2020-04-24 14:16:28 +0200

    trouble deploy?

--- app.js
+++ app.js
@@ -60,11 +60,10 @@ app.use('/', require('./routes/upload'));
 //   });
 // }
 
-app.use(express.static(path.join(__dirname, './build')))
-
+app.use(express.static('client/build'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-})
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
